3ab04580dc153a3259ed597c3b3ea389235f7b60
seojun2006 <papa897911@gmail.com>
2025-04-11 10:41:35 +0900

    202510804박서준 과제4

---
+++ hw4.py
@@ -0,0 +1,17 @@
+def rep_char(c, n):
+    return c * n
+
+def welcome_message():
+    name = input("Input his/her name: ")
+    msg1 = f"Hello {name},"
+    msg2 = "Welcome to Seoul."
+
+    nstr = len(msg1) if len(msg1) > len(msg2) else len(msg2)
+    line = rep_char('-', nstr)
+
+    print(line)
+    print(msg1)
+    print(msg2)
+    print(line)
+
+welcome_message()
